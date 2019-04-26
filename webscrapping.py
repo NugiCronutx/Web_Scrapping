6e4ca2d3babeac9f171d906bee7cc56e1c2bfadb
muhammad anugrah mulya <aank35165@gmail.com>
2019-04-26 23:31:08 +0800

    adding list and loop

--- webscrapping.py
+++ webscrapping.py
@@ -13,3 +13,10 @@ print(items[0].find(class_='period-name').get_text())
 print(items[0].find(class_='short-desc').get_text())
 print(items[0].find(class_='temp').get_text())
 
+period_names = [item.find(class_='period-name').get_text() for item in items]
+short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
+temperatures = [item.find(class_='temp').get_text() for item in items]
+
+print(period_names)
+print(short_descriptions)
+print(temperatures)
