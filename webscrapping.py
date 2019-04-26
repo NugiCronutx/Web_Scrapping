6e63039237d7d959b6da46491ddce527d8abe990
muhammad anugrah mulya <aank35165@gmail.com>
2019-04-26 23:52:58 +0800

    done beautifully

--- webscrapping.py
+++ webscrapping.py
@@ -1,3 +1,4 @@
+import pandas 
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,14 +10,26 @@ week = soup.find(id="seven-day-forecast-body")
 items = week.find_all(class_='tombstone-container')
 #print(items[0])
 
-print(items[0].find(class_='period-name').get_text())
-print(items[0].find(class_='short-desc').get_text())
-print(items[0].find(class_='temp').get_text())
+#print(items[0].find(class_='period-name').get_text())
+#print(items[0].find(class_='short-desc').get_text())
+#print(items[0].find(class_='temp').get_text())
 
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
 
-print(period_names)
-print(short_descriptions)
-print(temperatures)
+#print(period_names)
+#print(short_descriptions)
+#print(temperatures)
+
+weather_stuff = pandas.DataFrame(
+    {
+        'period': period_names,
+        'short_descriptions': short_descriptions,
+        'temperatures': temperatures
+     })
+
+
+print(weather_stuff)
+
+weather_stuff.to_csv('weather.csv')
